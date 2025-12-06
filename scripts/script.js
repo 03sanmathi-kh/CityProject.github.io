12a6e768884f1eba36e2e8109dafe5760cd4fa1f
03sanmathi-kh <sanmathikh@gmail.com>
2025-12-06 18:09:39 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
+function toggleTheme() {
+  document.body.classList.toggle("light");
+  const btn = document.getElementById("toggleBtn");
+  btn.textContent = document.body.classList.contains("light")
+    ? "Switch to Dark Mode"
+    : "Switch to Light Mode";
+}
 
